29eb6b4cfb2f72bb152d22b3d1e12327b546b584
JaeyoungLim <gokoreas@gmail.com>
2019-01-08 12:11:21 +0100

    Add basic command loop timers and pose subscriber

--- hilbert_mapper/include/hilbert_mapper/hilbert_mapper.h
+++ hilbert_mapper/include/hilbert_mapper/hilbert_mapper.h
@@ -25,11 +25,6 @@
 #include <mavros_msgs/CommandBool.h>
 #include <mavros_msgs/AttitudeTarget.h>
 #include <std_srvs/SetBool.h>
-#include <gazebo_msgs/ModelStates.h>
-
-#define MODE_ROTORTHRUST  1
-#define MODE_BODYRATE     2
-#define MODE_BODYTORQUE   3
 
 using namespace std;
 using namespace Eigen;
@@ -38,8 +33,19 @@ class hilbertMapper
   private:
     ros::NodeHandle nh_;
     ros::NodeHandle nh_private_;
+    ros::Timer cmdloop_timer_, statusloop_timer_;
+    ros::Subscriber mavposeSub_;
 
-  public:
+    Eigen::Vector3d mavPos_;
+    Eigen::Vector4d mavAtt_;
+
+    void cmdloopCallback(const ros::TimerEvent& event);
+    void statusloopCallback(const ros::TimerEvent& event);
+
+    void mavposeCallback(const geometry_msgs::PoseStamped& msg);
+
+
+public:
     hilbertMapper(const ros::NodeHandle& nh, const ros::NodeHandle& nh_private);
     virtual ~ hilbertMapper();
 };
--- hilbert_mapper/src/hilbert_mapper_node.cpp
+++ hilbert_mapper/src/hilbert_mapper_node.cpp
@@ -9,8 +9,34 @@ hilbertMapper::hilbertMapper(const ros::NodeHandle& nh, const ros::NodeHandle& n
   nh_(nh),
   nh_private_(nh_private) {
 
+    cmdloop_timer_ = nh_.createTimer(ros::Duration(0.01), &hilbertMapper::cmdloopCallback, this); // Define timer for constant loop rate
+    statusloop_timer_ = nh_.createTimer(ros::Duration(1), &hilbertMapper::statusloopCallback, this); // Define timer for constant loop rate
+
+    mavposeSub_ = nh_.subscribe("/mavros/local_position/pose", 1, &hilbertMapper::mavposeCallback, this,ros::TransportHints().tcpNoDelay());
 
 }
 hilbertMapper::~hilbertMapper() {
   //Destructor
+}
+
+void hilbertMapper::cmdloopCallback(const ros::TimerEvent& event) {
+
+
+
+    ros::spinOnce();
+}
+
+void hilbertMapper::statusloopCallback(const ros::TimerEvent &event) {
+
+}
+
+void hilbertMapper::mavposeCallback(const geometry_msgs::PoseStamped& msg){
+    mavPos_(0) = msg.pose.position.x;
+    mavPos_(1) = msg.pose.position.y;
+    mavPos_(2) = msg.pose.position.z;
+    mavAtt_(0) = msg.pose.orientation.w;
+    mavAtt_(1) = msg.pose.orientation.x;
+    mavAtt_(2) = msg.pose.orientation.y;
+    mavAtt_(3) = msg.pose.orientation.z;
+
 }
