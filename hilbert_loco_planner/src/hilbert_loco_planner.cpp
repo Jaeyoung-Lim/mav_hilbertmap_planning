952c6a89e8e99683fd99a2c1854dc9e62fbfc024
Jaeyoung Lim <jalim@student.ethz.ch>
2019-01-30 10:47:14 +0100

    Add template to loco_planner

--- hilbert_loco_planner/src/hilbert_loco_planner.cpp
+++ hilbert_loco_planner/src/hilbert_loco_planner.cpp
@@ -6,8 +6,9 @@ using namespace std;
 //Constructor
 HilbertLocoPlanner::HilbertLocoPlanner(const ros::NodeHandle& nh, const ros::NodeHandle& nh_private):
   nh_(nh),
-  nh_private_(nh_private)
-{
+  nh_private_(nh_private),
+  loco_(3){
+
 }
 HilbertLocoPlanner::~HilbertLocoPlanner() {
   //Destructor
