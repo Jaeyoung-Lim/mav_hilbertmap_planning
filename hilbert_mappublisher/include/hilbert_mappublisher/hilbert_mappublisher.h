92874d4b742373a0206800fd0b8203bb21f1bcc8
Jaeyoung Lim <jalim@student.ethz.ch>
2019-02-01 11:53:13 +0100

    Publish pose

--- hilbert_mappublisher/include/hilbert_mappublisher/hilbert_mappublisher.h
+++ hilbert_mappublisher/include/hilbert_mappublisher/hilbert_mappublisher.h
@@ -1,9 +1,11 @@
 //  July/2018, ETHZ, Jaeyoung Lim, jalim@student.ethz.ch
 
-#ifndef HILBERT_MAPPER_H
-#define HILBERT_MAPPER_H
+#ifndef HILBERT_MAPPUBLISHER_H
+#define HILBERT_MAPPUBLISHER_H
 
 #include <ros/ros.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <Eigen/Dense>
 
 
 class hilbertMapPublisher
@@ -13,8 +15,14 @@ class hilbertMapPublisher
     ros::NodeHandle nh_private_;
 
     ros::Timer cmdloop_timer_;
+    ros::Publisher mapcenter_pub_;
+
+    Eigen::Vector3d mapcenter_;
+
+
 
     void cmdloopCallback(const ros::TimerEvent& event);
+    void pubMapCenter();
 
 public:
     hilbertMapPublisher(const ros::NodeHandle& nh, const ros::NodeHandle& nh_private);
--- hilbert_mappublisher/src/hilbert_mappublisher.cpp
+++ hilbert_mappublisher/src/hilbert_mappublisher.cpp
@@ -5,7 +5,8 @@
 //Constructor
 hilbertMapPublisher::hilbertMapPublisher(const ros::NodeHandle& nh, const ros::NodeHandle& nh_private):
   nh_(nh),
-  nh_private_(nh_private){
+  nh_private_(nh_private),
+  mapcenter_(Eigen::Vector3d::Zero()){
 
     cmdloop_timer_ = nh_.createTimer(ros::Duration(0.01), &hilbertMapPublisher::cmdloopCallback, this); // Define timer for constant loop rate
 }
@@ -17,4 +18,21 @@ hilbertMapPublisher::~hilbertMapPublisher() {
 void hilbertMapPublisher::cmdloopCallback(const ros::TimerEvent& event) {
 
     ros::spinOnce();
+}
+
+void hilbertMapPublisher::pubMapCenter() {
+
+    geometry_msgs::PoseStamped mapcenterpose_msg;
+
+    mapcenterpose_msg.header.stamp = ros::Time::now();
+    mapcenterpose_msg.header.frame_id = "world";
+    mapcenterpose_msg.pose.position.x = mapcenter_(0);
+    mapcenterpose_msg.pose.position.y = mapcenter_(1);
+    mapcenterpose_msg.pose.position.z = mapcenter_(2);
+    mapcenterpose_msg.pose.orientation.w = 1.0;
+    mapcenterpose_msg.pose.orientation.w = 0.0;
+    mapcenterpose_msg.pose.orientation.w = 0.0;
+    mapcenterpose_msg.pose.orientation.w = 0.0;
+
+    mapcenter_pub_.publish(mapcenterpose_msg);
 }
