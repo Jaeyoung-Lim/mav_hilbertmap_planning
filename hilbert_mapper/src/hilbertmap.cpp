84e58fc4b2f68c7f3a763463e2a452c8933c9686
Jaeyoung Lim <jalim@student.ethz.ch>
2019-02-05 14:56:05 +0100

    Decrease the number of unneeded iterations

--- hilbert_mapper/src/hilbertmap.cpp
+++ hilbert_mapper/src/hilbertmap.cpp
@@ -7,7 +7,7 @@
 hilbertmap::hilbertmap(int num_features):
     num_features_(num_features),
     num_samples_(30),
-    max_iterations_(100),
+    max_iterations_(30),
     weights_(Eigen::VectorXd::Zero(num_features)),
     A_(Eigen::MatrixXd::Identity(num_features, num_features)),
     eta_(0.7),
