4ee9f3c3bbb28b6c2d1fc9cd37cd3e59423fcd1a
Jaeyoung Lim <jalim@student.ethz.ch>
2019-04-15 13:16:49 +0200

    Variable shape for hilbertmaps

--- hilbert_evaluator/src/hilbert_evaluator.cpp
+++ hilbert_evaluator/src/hilbert_evaluator.cpp
@@ -14,8 +14,17 @@ HilbertEvaluator::HilbertEvaluator(const ros::NodeHandle& nh, const ros::NodeHan
     cmdloop_timer_ = nh_.createTimer(ros::Duration(1.0), &HilbertEvaluator::cmdloopCallback, this); // Define timer for constant loop rate
     statusloop_timer_ = nh_.createTimer(ros::Duration(0.5), &HilbertEvaluator::statusloopCallback, this); // Define timer for constant loop rate
     Eigen::Vector3d cow_and_lady_center;
-    cow_and_lady_center << 0.0, 0.0, 0.0;
-    hilbert_mapper_.getHilbertMapPtr()->setMapCenter(cow_and_lady_center, 10.0, 10.0, 10.0);
+    cow_and_lady_center << 0.0, 0.0, 1.0;
+
+    int num_samples = 100;
+    double width = 6.0;
+    double length = 6.0;
+    double height = 3.0;
+    double resolution = 0.6;
+    double tsdf_threshold = 0.0;
+
+    hilbert_mapper_.getHilbertMapPtr()->setMapProperties(num_samples, width, length, height, resolution, tsdf_threshold);
+    hilbert_mapper_.getHilbertMapPtr()->setMapCenter(cow_and_lady_center, width, length, height);
 }
 
 HilbertEvaluator::~HilbertEvaluator() {
